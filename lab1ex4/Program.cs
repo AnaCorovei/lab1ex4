f03dbc2b4059d35b1c736a1b8d0b4cdb2de36ff0
Andrei Corovei <andrei.corovei@qubiz.com>
2023-08-19 11:53:55 +0300

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Ex4
+            Scrieti un program care va afisa semnul unui numar citit de la tastatura
+            • Daca numarul este pozitiv, va afisa “+”
+            • Daca numarul este negativ, va afisa “-”
+            • Daca numarul este 0, va afisa “0”*/
+
+            long number;
+
+            Console.WriteLine("Enter the number: ");
+            number = long.Parse(Console.ReadLine());
+
+            if (number > 0)
+            Console.WriteLine("The number is positive");
+
+            else if (number < 0)
+            {
+                Console.WriteLine("The number is negative");
+            }
+
+            else if (number == 0)
+            { 
+                Console.WriteLine(0);
+            }
+        }
+    }
+}
